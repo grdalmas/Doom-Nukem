0455381178180cc9495b79cd4a252b2c3fb56bfa
Celeste MARTINEZ <cmartine@e3r10p12.42.fr>
2019-03-02 00:59:54 +0100

    door+ elev

--- src/movement.c
+++ src/movement.c
@@ -6,7 +6,7 @@
 /*   By: grdalmas <grdalmas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 01:01:59 by bbataini          #+#    #+#             */
-/*   Updated: 2019/03/01 23:15:45 by bbataini         ###   ########.fr       */
+/*   Updated: 2019/03/02 00:40:04 by cmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ void			elevator(t_struct *p)
 {
 	if (p->keypress[KEY_1] == 1 || p->keypress[KEY_2] == 1
 			|| p->keypress[KEY_3] == 1 || p->keypress[KEY_4] == 1
-			|| p->keypress[KEY_5] == 1)
+			|| p->keypress[KEY_5] == 1 || p->keypress[KEY_PAD_1] == 1)
 	{
-		
+
 		if (p->sound == 1)
 			system("afplay ./Musiques/elevator_beep.mp3 &");
-			if (p->k == 7)
+		if (p->k == 7)
 			p->h = 0;
-		if (p->keypress[KEY_1] == 1)
+		if (p->keypress[KEY_1] == 1 || p->keypress[KEY_PAD_1] == 1)
 			p->k = 7;
-		else if (p->keypress[KEY_2] == 1)
+		else if (p->keypress[KEY_2] == 1 || p->keypress[KEY_PAD_2] == 1)
 			p->k = 0;
-		else if (p->keypress[KEY_3] == 1)
+		else if (p->keypress[KEY_3] == 1 || p->keypress[KEY_PAD_3] == 1)
 			p->k = 5;
-		else if (p->keypress[KEY_4] == 1)
+		else if (p->keypress[KEY_4] == 1 || p->keypress[KEY_PAD_4] == 1)
 			p->k = 8;
-		else if (p->keypress[KEY_5] == 1)
+		else if (p->keypress[KEY_5] == 1 || p->keypress[KEY_PAD_5] == 1)
 			p->k = 9;
 		p->elevator = 0;
 		p->c->p_x = 6.5;
@@ -81,7 +81,12 @@ static void		move_up3(t_struct *p, int move)
 			system("killall afplay");
 		}
 	}
-	if (move == 21)
+	else if (move == 24 && p->k == 2)
+	{
+		//			p->k = 0;
+		p->c->p_y -= 3;
+	}
+	else if (move == 21)
 		close_window(p);
 }
 
@@ -116,179 +121,182 @@ static void		move_up2(t_struct *p, int move)
 
 int			against_glitch_straff(t_struct * p, int move, double s)
 {
-		if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y))] == 0)
+	if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y))] == 0)
+	{
+		if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0
+				&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
 		{
-			if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0
-					&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
+			if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) + 1] == 0)
 			{
-				if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) + 1] == 0)
-				{
-					printf("y a un coin 1!!!\n");
-					if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x + 1
-							|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y + 1)
-						move = 1;
-				}
+				printf("y a un coin 1!!!\n");
+				if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x + 1
+						|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y + 1)
+					move = 1;
 			}
 		}
-			if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
-					&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
-			{
-				if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) - 1] == 0)
-				{
-					printf("y a un coin 2!!!\n");
-					if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x - 1
-							|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y - 1)
-						move = 1;
-				}
-			}
-			if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
-					&& (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0))
-			{
-				if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) - 1] == 0)
-				{
-					printf("y a un coin 3!!!\n");
-					if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x - 1
-							|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y - 1)
-						move = 1;
-				}
-			}
-			if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0
-					&& (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0))
-			{
-				if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) + 1] == 0)
-				{
-					printf("y a un coin 4!!!\n");
-					if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x - 1
-							|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y + 1)
-						move = 1;
-				}
-			}
+	}
+	if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
+			&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
+	{
+		if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) - 1] == 0)
+		{
+			printf("y a un coin 2!!!\n");
+			if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x - 1
+					|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y - 1)
+				move = 1;
+		}
+	}
+	if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
+			&& (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0))
+	{
+		if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) - 1] == 0)
+		{
+			printf("y a un coin 3!!!\n");
+			if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x - 1
+					|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y - 1)
+				move = 1;
+		}
+	}
+	if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0
+			&& (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0))
+	{
+		if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) + 1] == 0)
+		{
+			printf("y a un coin 4!!!\n");
+			if ((int)(p->c->p_x + p->c->plane_x * s) == (int)p->c->p_x - 1
+					|| (int)(p->c->p_y + p->c->plane_y * s) == (int)p->c->p_y + 1)
+				move = 1;
+		}
+	}
 	return (move);
 }
 
 int			against_glitch(t_struct * p, int move, double s)
 {
-		if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y))] == 0)
+	if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y))] == 0)
+	{
+		if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0
+				&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
 		{
-			if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0
-					&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
+			if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) + 1] == 0)
 			{
-				if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) + 1] == 0)
-				{
-					printf("y a un coin 1!!!\n");
-					if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x + 1
-							|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y + 1)
-						move = 1;
-				}
+				printf("y a un coin 1!!!\n");
+				if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x + 1
+						|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y + 1)
+					move = 1;
 			}
 		}
-			if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
-					&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
-			{
-				if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) - 1] == 0)
-				{
-					printf("y a un coin 2!!!\n");
-					if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x - 1
-							|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y - 1)
-						move = 1;
-				}
-			}
-			if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
-					&& (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0))
-			{
-				if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) - 1] == 0)
-				{
-					printf("y a un coin 3!!!\n");
-					if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x - 1
-							|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y - 1)
-						move = 1;
-				}
-			}
-			if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0
-					&& (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0))
-			{
-				if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) + 1] == 0)
-				{
-					printf("y a un coin 4!!!\n");
-					if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x - 1
-							|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y + 1)
-						move = 1;
-				}
-			}
+	}
+	if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
+			&& (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y))] > 0))
+	{
+		if (p->map[p->k][(int)((p->c->p_x)) + 1][(int)((p->c->p_y)) - 1] == 0)
+		{
+			printf("y a un coin 2!!!\n");
+			if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x - 1
+					|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y - 1)
+				move = 1;
+		}
+	}
+	if (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) - 1] > 0
+			&& (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0))
+	{
+		if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) - 1] == 0)
+		{
+			printf("y a un coin 3!!!\n");
+			if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x - 1
+					|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y - 1)
+				move = 1;
+		}
+	}
+	if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y))] > 0
+			&& (p->map[p->k][(int)((p->c->p_x))][(int)((p->c->p_y)) + 1] > 0))
+	{
+		if (p->map[p->k][(int)((p->c->p_x)) - 1][(int)((p->c->p_y)) + 1] == 0)
+		{
+			printf("y a un coin 4!!!\n");
+			if ((int)(p->c->p_x + p->c->dir_x * s) == (int)p->c->p_x - 1
+					|| (int)(p->c->p_y + p->c->dir_y * s) == (int)p->c->p_y + 1)
+				move = 1;
+		}
+	}
 	return (move);
 }
 
 
-		void			move_up(t_struct *p, int move, int i, double s)
+void			move_up(t_struct *p, int move, int i, double s)
+{
+	int t;
+	int tmp;
+
+	t = 0;
+	tmp = 0;
+	while (t < NUMPORTE)
+	{
+		if (p->porte[t].zip == move)
+			break ;
+		t++;
+	}
+	if (move == 0 && (int)p->c->p_x == 7 && (int)p->c->p_y == 7
+			&& p->k != 1 && p->k != 2 && p->k != 3 && p->k != 4)
+	{
+		if (p->k == 0)
+			p->elevator = 2;
+		if (p->k == 5)
+			p->elevator = 3;
+		if (p->k == 7)
+			p->elevator = 1;
+		if (p->k == 8)
+			p->elevator = 4;
+		if (p->k == 9)
+			p->elevator = 5;
+		elevator(p);
+	}
+	if (p->k == 4)
+	{
+		if (move == 0 && (p->keypress[KEY_A] == 1 || p->keypress[KEY_D] == 1))
+			move = against_glitch_straff(p, move, s);
+		else
+			move = against_glitch(p, move, s);
+	}
+	if (move == 0 && (p->keypress[KEY_A] == 1 || p->keypress[KEY_D] == 1))
+	{
+		//		write(1, "1\n", 2);
+		while (i < 5)
 		{
-			int t;
-			int tmp;
+			if (p->map[p->k][(int)((p->c->p_x + p->c->plane_x * i * s / 4))]
+					[(int)((p->c->p_y + p->c->plane_y * i * s / 4))] == 1)
+				break ;
+			if (++i == 5)
+			{
+				p->c->p_x += p->c->plane_x * s;
+				p->c->p_y += p->c->plane_y * s;
+			}
+		}
+	}
 
-			t = 0;
-			tmp = 0;
-			while (t < NUMPORTE)
+	if (move == 0 || (t < NUMPORTE && p->porte[t].open >= 0.6))
+	{
+		//		write(1, "2\n", 2);
+		//	if ((p->c->middle_wall_dist > 0.5) && )
+		while (i < 5)
+		{
+			if (p->map[p->k][(int)((p->c->p_x + p->c->dir_x * i * s / 4))]
+					[(int)((p->c->p_y + p->c->dir_y * i * s / 4))] == 1)
+				break ;
+			if (++i == 5)
 			{
-				if (p->porte[t].zip == move)
-					break ;
-				t++;
+				p->c->p_x += p->c->dir_x * s;
+				p->c->p_y += p->c->dir_y * s;
 			}
-			if (move == 0 && (int)p->c->p_x == 7 && (int)p->c->p_y == 7
-					&& p->k != 1 && p->k != 2 && p->k != 3 && p->k != 4)
-			{
-				if (p->k == 0)
-					p->elevator = 2;
-				if (p->k == 5)
-					p->elevator = 3;
-				if (p->k == 7)
-					p->elevator = 1;
-				if (p->k == 8)
-					p->elevator = 4;
-				if (p->k == 9)
-					p->elevator = 5;
-				elevator(p);
-			}
-
-			if (move == 0 && (p->keypress[KEY_A] == 1 || p->keypress[KEY_D] == 1) && p->k == 4)
-				move = against_glitch_straff(p, move, s);
-			if (move == 0 && (p->keypress[KEY_A] == 1 || p->keypress[KEY_D] == 1))
-			{
-				//		write(1, "1\n", 2);
-				while (i < 5)
-				{
-					if (p->map[p->k][(int)((p->c->p_x + p->c->plane_x * i * s / 4))]
-							[(int)((p->c->p_y + p->c->plane_y * i * s / 4))] == 1)
-						break ;
-					if (++i == 5)
-					{
-						p->c->p_x += p->c->plane_x * s;
-						p->c->p_y += p->c->plane_y * s;
-					}
-				}
-			}
-			if (p->k == 4)
-				move = against_glitch(p, move, s);
-			if (move == 0 || (t < NUMPORTE && p->porte[t].open >= 0.6))
-			{
-				//		write(1, "2\n", 2);
-				//	if ((p->c->middle_wall_dist > 0.5) && )
-				while (i < 5)
-				{
-					if (p->map[p->k][(int)((p->c->p_x + p->c->dir_x * i * s / 4))]
-							[(int)((p->c->p_y + p->c->dir_y * i * s / 4))] == 1)
-						break ;
-					if (++i == 5)
-					{
-						p->c->p_x += p->c->dir_x * s;
-						p->c->p_y += p->c->dir_y * s;
-					}
-				}
-			}
-	else if (move == 12 || move == 13 || move == 68 || move == 66)
+		}
+	}
+	else if (move == 12 || move == 13 || move == 68 || move == 66 || move == 24)
 	{
 		move_up2(p, move);
 		if (p->sound == 1)
-		system("afplay ./musics/tp.mp3 &");
+			system("afplay ./musics/tp.mp3 &");
 		else
 			system("killall afplay");
 	}
-	}
+}
